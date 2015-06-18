1e12cd225c354916e64ec685e68fdb3bb83590e0
Simon Højberg <r.hackr@gmail.com>
2015-06-18 20:55:44 +0200

    Add 1.3 video content

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+class Header extends React.Component {
+  render() {
+    return <h1 style={{color: 'red'}}>{this.props.children}</h1>;
+  }
+}
+
+class Greeter extends React.Component {
+  render() {
+    return <Header>Hello {this.props.name}</Header>;
+  }
+}
+
 class App extends React.Component {
   render() {
-    return <h1>Hello World!</h1>;
+    return <Greeter name='Bruce Wayne' />;
   }
 }
 
