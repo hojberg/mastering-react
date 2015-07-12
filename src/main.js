3ffcfa2011495cd2bdd33ffc87e6afa6bef7129a
Simon Højberg <r.hackr@gmail.com>
2015-07-12 15:01:24 +0200

    Render app with react-dom

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './components/app';
 
-React.render(<App />, document.querySelector('#anchor'));
+ReactDOM.render(<App />, document.querySelector('#anchor'));
