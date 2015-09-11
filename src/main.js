0296ec202d1b05226d9cfabbdffde887d0a2313b
Simon Højberg <r.hackr@gmail.com>
2015-09-10 21:45:39 -0400

    React 0.14

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class Header extends React.Component {
   render() {
@@ -63,4 +64,4 @@ class App extends React.Component {
   }
 }
 
-React.render(<App />, document.querySelector('#app'));
+ReactDOM.render(<App />, document.querySelector('#app'));
