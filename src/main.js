5ec7e089cfba25f53cdbf911d4cdda34cf995cc2
Simon Højberg <r.hackr@gmail.com>
2015-09-10 21:39:11 -0400

    react 0.14

--- src/main.js
+++ src/main.js
@@ -1,21 +1,24 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class Header extends React.Component {
   render() {
-    return <h1 style={{color: 'red'}}>{this.props.children}</h1>;
+    return <h1 style={{color: 'red'}}>{this.props.children}</h1>
   }
 }
 
 class Greeter extends React.Component {
   render() {
-    return <Header>Hello {this.props.name}</Header>;
+    return <Header>Hello {this.props.name}</Header>
   }
 }
+
+const GreeterFactory = React.createFactory(Greeter);
 
 class App extends React.Component {
   render() {
-    return <Greeter name='Bruce Wayne' />;
+    return <Greeter name="Bruce Wayne" />;
   }
 }
 
-React.render(<App />, document.querySelector('#app'));
+ReactDOM.render(<App />, document.querySelector("#app"));
